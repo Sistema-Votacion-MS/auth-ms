bd33b7dd8a2cbbee2cbf167b64d1c6819fcfead5
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-07-04 18:12:04 -0500

    feat: enhance environment variable validation and add NATS_SERVICE constant

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,13 +3,20 @@ import * as Joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  JWT_SECRET: string;
+  NATS_SERVERS: string[];
 }
 
 const envSchema = Joi.object({
   PORT: Joi.number().required(),
+  JWT_SECRET: Joi.string().required(),
+  NATS_SERVERS: Joi.array().items(Joi.string()).required(),
 }).unknown(true);
 
-const { error, value } = envSchema.validate(process.env);
+const { error, value } = envSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+});
 
 if (error) {
   throw new Error(`Environment variable validation error: ${error.message}`);
@@ -19,4 +26,6 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+  jwtSecret: envVars.JWT_SECRET,
+  natsServers: envVars.NATS_SERVERS,
 };
